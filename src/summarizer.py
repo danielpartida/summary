01997f13f8ae60b5f576eaa4289f1e9b929c76f1
danielpartida <daniel.partida@d-fine.de>
2022-04-28 18:05:48 +0200

    Add sources

--- src/summarizer.py
+++ src/summarizer.py
@@ -1,7 +1,13 @@
+# https://huggingface.co/tasks/summarization
+# https://huggingface.co/docs/transformers/tasks/summarization
+# https://huggingface.co/course/chapter7/5?fw=pt
+
 from transformers import pipeline
 
 classifier = pipeline("summarization")
 
-file = open("../text/crypto_com_processed.txt", "r")
+file = open("../text/crypto_com_processed.txt", "r", encoding='mbcs')
 text = file.read()
-print(text)
+
+summary = classifier(text)
+print(summary)
